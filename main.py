30d4e3283254777f3387e679d0ba28920b8d84fd
banbao <ilya.kruglove@yandex.ru>
2024-01-17 20:41:55 +0300

    Test Enum choices

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from enum import Enum
 from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+class EducationLevel(str, Enum):
+    SECONDARY = 'Среднее образование'
+    SPECIAL = 'Среднее специальное образование'
+    HIGHER = 'Высшее образование'
 
 
 @app.get("/multiplication")
@@ -27,12 +34,15 @@ def greetings(
         name: str,
         surname: str,
         age: Optional[int] = None,
-        is_staff: bool = False
+        is_staff: bool = False,
+        education_level: Optional[EducationLevel] = None,
 ) -> dict[str, str]:
     result = " ".join([name, surname])
     result = result.title()
     if age is not None:
         result += ", " + str(age)
+    if education_level is not None:
+        result += ", " + education_level.lower()
     if is_staff:
         result += ", сотрудник"
     return {"Hello": result}
