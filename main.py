242812da8e4c2eed176864811905a9051c648973
banbao <ilya.kruglove@yandex.ru>
2024-01-17 19:40:02 +0300

    Test path parameter

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ import uvicorn
 
 from fastapi import FastAPI
 
-# Создание объекта приложения.
-# app = FastAPI()
-# Без документации (uvicorn main:app --reload)
-# app = FastAPI(docs_url=None, redoc_url=None)
-app = FastAPI(docs_url='/swagger')
+app = FastAPI()
 
 
-# Декоратор, определяющий, что GET-запросы к основному URL приложения
-# должны обрабатываться этой функцией.
-@app.get('/')
-def read_root():
-    return {'Hello': 'FastAPI'}
+@app.get("/{name}")
+def greetings(name):
+    return {"Hello": name}
 
 
 if __name__ == '__main__':
-    # Команда на запуск uvicorn.
-    # Здесь же можно указать хост и/или порт при необходимости,
-    # а также другие параметры.
     uvicorn.run('main:app', reload=True)
