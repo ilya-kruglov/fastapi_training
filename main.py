106fef54c2c1d317d8f0d5cede07aee87f8faa79
banbao <ilya.kruglove@yandex.ru>
2024-01-17 21:25:56 +0300

    Test IntEnum choices (CelestialBodies)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, IntEnum
 from typing import Optional
 
 import uvicorn
@@ -11,6 +11,25 @@ class EducationLevel(str, Enum):
     SECONDARY = 'Среднее образование'
     SPECIAL = 'Среднее специальное образование'
     HIGHER = 'Высшее образование'
+
+
+class CelestialBodies(IntEnum):
+    SUN = 1_392_000
+    JUPITER = 139_822
+    SATURN = 116_464
+    URANUS = 50_724
+    NEPTUNE = 49_224
+    EARTH = 12_742
+    VENUS = 12_104
+    MARS = 6_780
+    GANYMEDE = 5_262
+    TITAN = 5_151
+    MERCURY = 4_879
+
+
+@app.get('/get-solar-object-name')
+def get_solar_object_name(diameter: CelestialBodies) -> str:
+    return CelestialBodies(diameter).name
 
 
 @app.get("/multiplication")
