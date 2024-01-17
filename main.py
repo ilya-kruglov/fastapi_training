4d74f49dd3312f584edf865afe14ea00ebc2ae8f
banbao <ilya.kruglove@yandex.ru>
2024-01-17 19:44:12 +0300

    Test func place order

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# @app.get("/me")
+# def hello_author():
+#     return {"Hello": "author"}
+
+
 @app.get("/{name}")
 def greetings(name):
     return {"Hello": name}
+
+
+@app.get("/me")
+def hello_author():
+    return {"Hello": "author"}
 
 
 if __name__ == '__main__':
