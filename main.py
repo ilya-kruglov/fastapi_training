987f40c178509ba02fa0b76dadc7d06737b0aa77
banbao <ilya.kruglove@yandex.ru>
2024-01-17 19:47:00 +0300

    Test type annotation

--- main.py
+++ main.py
@@ -5,19 +5,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-# @app.get("/me")
-# def hello_author():
-#     return {"Hello": "author"}
-
-
-@app.get("/{name}")
-def greetings(name):
-    return {"Hello": name}
-
-
 @app.get("/me")
 def hello_author():
     return {"Hello": "author"}
+
+
+@app.get("/{name}")
+def greetings(name: str):
+    return {"Hello": name.capitalize()}
 
 
 if __name__ == '__main__':
