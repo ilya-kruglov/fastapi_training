067459c0a9d1fc89cd3873e904e0144962f4cde4
banbao <ilya.kruglove@yandex.ru>
2024-01-19 21:26:45 +0300

    Add file upload functionality using the UploadFile class.

--- form.py
+++ form.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, File, Form, UploadFile
 
 
 app = FastAPI()
@@ -9,8 +9,13 @@ app = FastAPI()
 def login(
         username: str = Form(...),
         password: str = Form(...),
+        some_file: UploadFile = File(...)
 ):
-    return {'username': username}
+    file_content = some_file.file.read().splitlines()
+    return {
+        'username': username,
+        'file_content': file_content,
+    }
 
 
 if __name__ == '__main__':
