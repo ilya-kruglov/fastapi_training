308e941eb2ebe8a4ae0b18171cb0328cae9fe0e8
banbao <ilya.kruglove@yandex.ru>
2024-01-20 15:24:50 +0300

    Refactor code into multiple files: create schemas.py for Pydantic models and related classes

--- main.py
+++ main.py
@@ -1,39 +1,9 @@
-from enum import Enum, IntEnum
-from typing import Optional, Union
-
 import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from schemas import CelestialBodies, Person
 
 app = FastAPI()
-
-
-class EducationLevel(str, Enum):
-    SECONDARY = 'Среднее образование'
-    SPECIAL = 'Среднее специальное образование'
-    HIGHER = 'Высшее образование'
-
-
-class CelestialBodies(IntEnum):
-    SUN = 1_392_000
-    JUPITER = 139_822
-    SATURN = 116_464
-    URANUS = 50_724
-    NEPTUNE = 49_224
-    EARTH = 12_742
-    VENUS = 12_104
-    MARS = 6_780
-    GANYMEDE = 5_262
-    TITAN = 5_151
-    MERCURY = 4_879
-
-
-class Person(BaseModel):
-    name: str
-    surname: Union[str, list[str]]
-    age: Optional[int]
-    is_staff: bool = False
-    education_level: Optional[EducationLevel]
 
 
 @app.get('/get-solar-object-name')
---
+++ schemas.py
@@ -0,0 +1,32 @@
+from enum import Enum, IntEnum
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+
+class CelestialBodies(IntEnum):
+    SUN = 1_392_000
+    JUPITER = 139_822
+    SATURN = 116_464
+    URANUS = 50_724
+    NEPTUNE = 49_224
+    EARTH = 12_742
+    VENUS = 12_104
+    MARS = 6_780
+    GANYMEDE = 5_262
+    TITAN = 5_151
+    MERCURY = 4_879
+
+
+class EducationLevel(str, Enum):
+    SECONDARY = 'Среднее образование'
+    SPECIAL = 'Среднее специальное образование'
+    HIGHER = 'Высшее образование'
+
+
+class Person(BaseModel):
+    name: str
+    surname: Union[str, list[str]]
+    age: Optional[int]
+    is_staff: bool = False
+    education_level: Optional[EducationLevel]
